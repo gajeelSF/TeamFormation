6a0595a23c9cd933418924514887039205a55266
Xuanqiang Zhao <vsastray@Xuanqiangs-MacBook-Pro.local>
2017-04-01 01:12:10 -0700

    parse

--- app/src/main/java/com/example/android/teamformation/MainActivity.java
+++ app/src/main/java/com/example/android/teamformation/MainActivity.java
@@ -9,6 +9,7 @@ import android.app.Application;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import static android.R.attr.duration;
@@ -21,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("teamformation")
-                .server("http://teamformation.herokuapp.com/lahackteam")
+                .server("http://teamformation.herokuapp.com/lahackteam/parse")
                 .build()
         );
     }
@@ -37,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
         user.setUsername((String) usernameText.getText().toString());
         user.setPassword((String) passwordText.getText().toString());
 
+
+        System.out.println((String) usernameText.getText().toString());
+        System.out.println((String) passwordText.getText().toString());
         user.signUpInBackground(new SignUpCallback() {
             public void done(ParseException e) {
                 if (e == null) {
