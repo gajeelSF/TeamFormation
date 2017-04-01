4c84e1b338f3f4ad93caca66e2ee8eebf350efc0
zod zhao <jkjdavid@outlook.com>
2017-04-01 11:51:50 -0700

    create post activity

--- app/src/main/java/com/example/android/teamformation/Home.java
+++ app/src/main/java/com/example/android/teamformation/Home.java
@@ -1,5 +1,6 @@
 package com.example.android.teamformation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -28,8 +29,10 @@ public class Home extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                Intent post = new Intent(getApplicationContext(), PostActivity.class);
+                startActivity(post);
             }
         });
 
---
+++ app/src/main/java/com/example/android/teamformation/PostActivity.java
@@ -0,0 +1,48 @@
+package com.example.android.teamformation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.parse.*;
+import android.app.Application;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.R.attr.duration;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("teamfor")
+                .server("http://teamfor.herokuapp.com/parse/")
+                .build()
+        );
+    }
+
+    public void post(View view) {
+        final Context context = getApplicationContext();
+        Button button = (Button) findViewById(R.id.SignUpButton);
+
+        EditText emailText = (EditText) findViewById(R.id.email);
+
+        System.out.println((String) emailText.getText().toString());
+
+        ParseObject testObject = new ParseObject("Post");
+        testObject.put("content", emailText);
+        testObject.saveInBackground();
+
+
+
+    }
+}
